2227179ca20ad42145ae7f4401d2de45decf2c03
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-27 03:34:42 +0700

    SP-1-3 (Gagarin_data)

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_4.kt
@@ -0,0 +1,34 @@
+
+
+fun main() {
+
+    val length: Long = 40868600000
+    val age: Short = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    println(Long)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
