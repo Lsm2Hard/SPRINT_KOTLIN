fc414f099fd45e24e62d2c025ba394d1546cc2c5
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-27 03:10:23 +0700

    SP-1-3 (Space.exe)

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_3.kt
@@ -0,0 +1,33 @@
+
+
+fun main() {
+
+    val year: Int = 1963
+    var hour = "09"
+    var minute = "07"
+    println(year)
+    println(hour)
+    println(minute)
+
+     hour = "10:"
+     minute = "55"
+
+    print( hour)
+    print(minute)
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
