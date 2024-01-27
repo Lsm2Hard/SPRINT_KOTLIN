a524f96c2dc5b6d563588a4ce9dc00a7af8a5de4
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-27 22:48:40 +0700

    SP-1-5 (Dif_quest!)

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_5.kt
@@ -0,0 +1,36 @@
+
+
+fun main() {
+
+    var seconds: Int = 6480
+
+    //seconds = seconds / 60  //целые минуты 108
+
+    seconds: Double = 6480.0
+    seconds = seconds/60/60
+
+
+    println(seconds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
