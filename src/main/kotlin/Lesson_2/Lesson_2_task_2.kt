b16c7b399f236312ff8250defd9c825a6a087c10
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-27 23:37:43 +0700

    SP-2-2 Summ+average interns and employee

---
+++ src/main/kotlin/Lesson_2/Lesson_2_task_2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var employee = 50
+    var salaryEmploee = 30000
+    var interns = 30
+    var salaryInterns = 20000
+    var expenses = 50 * 30000
+    var generalExpenses = expenses + (interns*salaryInterns)
+    var average = generalExpenses / 80
+    println(expenses)
+    println(generalExpenses)
+    println(average)
+}
