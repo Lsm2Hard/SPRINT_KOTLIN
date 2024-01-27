8a8cac9767cdf37cb9cbddedbf0627eb71023903
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-28 02:37:25 +0700

    SP-3-1 Day|Night

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name = "Владимир"
+    var notification = " Добрый"
+    var day= "день"
+
+    println("$notification $day,  $name")
+
+    day = "вечер"
+
+    println("$notification  $day,  $name")
+
+
+
+
+}
+
