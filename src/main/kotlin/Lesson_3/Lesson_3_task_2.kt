9ac36d4dc836031ab48fa80219a36572829324e8
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-28 02:48:22 +0700

    SP-3-2 Women change second name

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_2.kt
@@ -0,0 +1,20 @@
+fun main() {
+    //var andreevnaTatyanaSergeevnaTwentyYear = "Андреева Татьяна Сергеевна, 20 лет"
+
+    var firstName = "Татьяна"
+    var secondName = "Андреевна"
+    var newSecondName = "Сидорова"
+    var lastName = "Сергеевна"
+
+    var twentyOld = 20
+    var twentyTwoOld = 22
+
+
+
+    println( "$secondName $firstName $lastName ${twentyOld} лет")
+    println( "$secondName $newSecondName $lastName ${twentyTwoOld} лет")
+
+
+
+}
+
