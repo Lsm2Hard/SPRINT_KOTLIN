4cc4647762e2854f92930c62e92ef2806509921a
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-28 03:19:38 +0700

    SP-3-3 Multiplication table

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_3.kt
@@ -0,0 +1,27 @@
+fun main() {
+    var number = 9
+
+
+    var numberFunc = """ 
+         
+    ${number *1}
+    ${number *2}
+    ${number *3}
+    ${number *4}
+    ${number *5}
+    ${number *6}
+    ${number *7}
+    ${number *8}
+    ${number *9}
+         """
+        
+
+println(numberFunc)
+
+
+
+
+
+
+}
+
