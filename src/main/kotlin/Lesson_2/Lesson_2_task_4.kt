52e3f81587379044e52c407df7280ccfafa46af2
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-28 00:53:07 +0700

    SP-2-4 Crystalline and iron ore

---
+++ src/main/kotlin/Lesson_2/Lesson_2_task_4.kt
@@ -0,0 +1,19 @@
+fun main() {
+ var crystall = 7
+  var iron = 11;
+
+  var bafCrystall = ((crystall * 100) /100 * 20 ) / 100
+  print("Колличество бонусов от бафа для кристаллической руды ")
+  println(bafCrystall)
+
+
+ var bafIron = ((iron * 100) /100 * 20 ) / 100
+ print("Колличество бонусов от бафа для железной руды ")
+ print(bafIron)
+
+
+
+
+
+
+}
