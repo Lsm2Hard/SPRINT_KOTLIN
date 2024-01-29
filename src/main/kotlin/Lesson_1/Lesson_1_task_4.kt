4b76511a40369da08726dac4ef390fd323d4362c
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-30 03:36:25 +0700

    SP-1-4 Data size (reworked)

--- src/main/kotlin/Lesson_1/Lesson_1_task_4.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_task_4.kt
@@ -1,28 +1,18 @@
-
-
 fun main() {
 
     val length: Long = 40868600000
-    val age: Short = 27
+    val age: Byte = 28
     val partOfDay: Float = 0.075f
-    val seconds: Int = 6480
+    val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apoge: Int = 327000
+
     println(Long)
     println(age)
     println(partOfDay)
     println(seconds)
     println(partOfYear)
     println(apoge)
-
-
-
-
-
-
-
-
-
 
 
 }
