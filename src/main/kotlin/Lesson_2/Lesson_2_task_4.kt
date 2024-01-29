292805a496a2a0b6e7b164771ff243338e3d8a83
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-30 04:20:44 +0700

    SP-2-4 search Buff (reworked)

--- src/main/kotlin/Lesson_2/Lesson_2_task_4.kt
+++ src/main/kotlin/Lesson_2/Lesson_2_task_4.kt
@@ -1,19 +1,17 @@
 fun main() {
- var crystall = 7
-  var iron = 11;
-
-  var bafCrystall = ((crystall * 100) /100 * 20 ) / 100
-  print("Колличество бонусов от бафа для кристаллической руды ")
-  println(bafCrystall)
+    val crystall = 7
+    val iron = 11
+    val addNubmer = 100
+    val precentOfBuff = 20
 
 
- var bafIron = ((iron * 100) /100 * 20 ) / 100
- print("Колличество бонусов от бафа для железной руды ")
- print(bafIron)
+    val bafCrystall = ((crystall * addNubmer) / addNubmer * precentOfBuff) / addNubmer
+    print("Колличество бонусов от бафа для кристаллической руды ")
+    println(bafCrystall)
 
-
-
-
+    val bafIron = ((iron * addNubmer) / addNubmer * precentOfBuff) / addNubmer
+    print("Колличество бонусов от бафа для железной руды ")
+    print(bafIron)
 
 
 }
