d905a3b6ae26ba9616876377bf551de763d95d53
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-30 04:42:13 +0700

    SP-3-4 chess (reworked)

--- src/main/kotlin/Lesson_3/Lesson_3_task_4.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_task_4.kt
@@ -3,32 +3,12 @@ fun main() {
     var from = "E2"
     var where = "E4"
     var numberMove = "1 ход"
-
-    //var all = from + where + numberMove
     println("$from $where, $numberMove")
 
     from = "D2"
     where = "D3"
     numberMove = "2 ход"
-
     println("$from $where, $numberMove")
-
-    var a: Int = 1
-    var b: Double = 2.2
-    var c = b + a
-    println(c)
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
