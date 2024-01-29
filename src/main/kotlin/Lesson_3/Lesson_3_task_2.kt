c841e0b4bceb8ded1351525b3950a3585f9b3820
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-30 04:35:36 +0700

    SP-3-2 Уears Old (reworked)

--- src/main/kotlin/Lesson_3/Lesson_3_task_2.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_task_2.kt
@@ -1,18 +1,17 @@
 fun main() {
-    //var andreevnaTatyanaSergeevnaTwentyYear = "Андреева Татьяна Сергеевна, 20 лет"
 
-    var firstName = "Татьяна"
+    val firstName = "Татьяна"
     var secondName = "Андреевна"
-    var newSecondName = "Сидорова"
-    var lastName = "Сергеевна"
+    val lastName = "Сергеевна"
+    var yearsOld = 20
 
-    var twentyOld = 20
-    var twentyTwoOld = 22
+    println("$secondName $firstName $lastName ${yearsOld} лет")
 
+    yearsOld = 22
+    secondName = "Сидорова"
 
+    println("$secondName $firstName $lastName ${yearsOld} лет")
 
-    println( "$secondName $firstName $lastName ${twentyOld} лет")
-    println( "$secondName $newSecondName $lastName ${twentyTwoOld} лет")
 
 
 
