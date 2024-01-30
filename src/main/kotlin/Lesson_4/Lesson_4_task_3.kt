ce93f94e4f9aa3a1eddec1af11129b165765fa23
Vladimir Serdyuk <jeer96@mail.ru>
2024-01-31 04:39:49 +0700

    SP-4-3 Agrocomplex

---
+++ src/main/kotlin/Lesson_4/Lesson_4_task_3.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val weatherTodaySolar: Boolean = true
+    val tentOpen: Boolean = true
+    val airHumidity: Int = 20
+    val currentTimeOfYear: String = "Winte"
+
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? ${
+            (weatherTodaySolar == weatherTodaySolar) && (tentOpen == tentOpen) && (airHumidity == airHumidity) && !(currentTimeOfYear == currentTimeOfYear)
+        }"
+    )
+
+
+}
+
