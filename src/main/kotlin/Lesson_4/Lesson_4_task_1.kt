da3ac1c0e3bc5853ccdde2a940de3d0c0f05c6ab
Vladimir Serdyuk <jeer96@mail.ru>
2024-02-03 04:55:30 +0700

    SP-4-1 ALL_TABLE (RE)

--- src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
+++ src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
@@ -3,7 +3,8 @@ fun main() {
     val preorderToday = 13
     val preorderTommorow = 9
 
-    println("Доступность столиков на сегодня ${preorderToday < allTable} \nДоступность столиков на завтра ${preorderTommorow < allTable}")
+    println("Доступность столиков на сегодня ${preorderToday < ALL_TABLE} " +
+            "\nДоступность столиков на завтра ${preorderTommorow < ALL_TABLE}")
 }
 
-const val allTable = 13
+const val ALL_TABLE = 13
---
+++ src/main/kotlin/Lesson_6/Lesson_6_1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val userName = readLine()
+
+
+
+    while  (   userName !=  Login  ) {
+        println("asdasd")
+
+
+    }
+
+
+
+
+
+}
+
+const val Login = "Jack"
+const val Pas = "Ter"
