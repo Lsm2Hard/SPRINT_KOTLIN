6d7d568d78b9ea9c1cdcda27386b0450dbfaae46
Vladimir Serdyuk <jeer96@mail.ru>
2024-02-03 05:37:16 +0700

    SP-4-3 Agrocomplex  (RE)

--- src/main/kotlin/Lesson_4/Lesson_4_task_3.kt
+++ src/main/kotlin/Lesson_4/Lesson_4_task_3.kt
@@ -1,16 +1,25 @@
 fun main() {
+    val weatherSolar = true
+    val tentOpen = true
+    val nowAirHummidity = 220
+    val currentTimeOfYearNow = "Зима"
 
-    val weatherTodaySolar: Boolean = true
-    val tentOpen: Boolean = true
-    val airHumidity: Int = 20
-    val currentTimeOfYear: String = "Winte"
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            (weatherTodaySolar == weatherTodaySolar) && (tentOpen == tentOpen) && (airHumidity == airHumidity) && !(currentTimeOfYear == currentTimeOfYear)
+            (WEATHER_SOLAR == weatherSolar) && (NOW_TENT_OPEN == tentOpen)
+                    && (AIR_HUMIDITY == nowAirHummidity)
+                    && (CURRENT_TIME_OF_YEAR != currentTimeOfYearNow)
         }"
     )
 
 
 }
+
+const val WEATHER_SOLAR = true
+const val NOW_TENT_OPEN: Boolean = true
+const val AIR_HUMIDITY: Int = 20
+const val CURRENT_TIME_OF_YEAR: String = "Не зима"
+
+
 
